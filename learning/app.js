cf011be6e5bcd7ada6fbc487a4e2bb4336e5f96d
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-11 21:32:55 -0500

    Add basic JavaScript demo and HTML page
    Introduces a simple JavaScript file demonstrating variable types, user prompts, and switch statements. Adds an HTML page that loads the script and includes Bootstrap for styling.

---
+++ learning/app.js
@@ -0,0 +1,31 @@
+// Variable
+let x = 10;
+alert(typeof x);
+
+let y = "Bonjour";
+alert(typeof y);
+
+let name = prompt("What is your Name : ");
+
+alert(`my name is ${name}.`);
+
+console.log(`My name is ${name}`);
+
+let age = prompt("How old are you?");
+
+switch (age) {
+  case 10:
+    alert("You are young");
+    break;
+  case 20:
+    alert("You are old");
+    break;
+  case 30:
+    alert("You are old old");
+    break;
+  case 50:
+    alert("You are too old");
+    break;
+  default:
+    alert("Your age is not in the range");
+}
