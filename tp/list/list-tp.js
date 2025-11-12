b78634bf39dada2a5d883eec04104d40d12aa8cb
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-11 23:51:14 -0500

    Add input and button to shopping list UI
    Introduces an input field and 'Ajouter' button to allow users to add new items to the shopping list. Refactors list rendering into a function and updates the DOM structure for improved interactivity.

--- tp/list/list-tp.js
+++ tp/list/list-tp.js
@@ -1,24 +1,55 @@
 let listeCourse = ["Orange", "Tartinade", "Bananes", "Brioches", "Pommes"];
 
 var mainContent = document.getElementById("page");
+mainContent.style.padding = "8px";
 
 var ulElement = document.createElement("ul");
 
 var h1Element = document.createElement("h1");
 h1Element.textContent = "Liste des coures :";
 
-ulElement.appendChild(h1Element);
+var inputElement = document.createElement("input");
+inputElement.id = "name";
+inputElement.type = "text";
+inputElement.value = "";
 
-for (let i = 0; i < listeCourse.length; i++) {
-  var liElement = document.createElement("li");
-  var liContent = (document.createTextNode = listeCourse[i]);
-  liElement.textContent = liContent;
+var addBtn = document.createElement("button");
+addBtn.textContent = "Ajouter";
+addBtn.style.marginLeft = "5px";
+addBtn.id = "addBtn";
 
-  if (i % 2 == 0) {
-    liElement.style.color = "blue";
+mainContent.appendChild(h1Element);
+mainContent.appendChild(inputElement);
+mainContent.appendChild(addBtn);
+
+addBtn.addEventListener("click", () => {
+  if (!inputElement.value.trim()) {
+    alert("Enter an article to buy");
+  } else {
+    listeCourse.push(inputElement.value);
+    inputElement.value = "";
+    console.log(listeCourse);
+
+    reRenderList();
+  }
+});
+
+function reRenderList() {
+  ulElement.innerHTML = "";
+
+  for (let i = 0; i < listeCourse.length; i++) {
+    var liElement = document.createElement("li");
+    var liContent = (document.createTextNode = listeCourse[i]);
+    liElement.textContent = liContent;
+
+    if (i % 2 == 0) {
+      liElement.style.color = "blue";
+    }
+
+    ulElement.appendChild(liElement);
   }
 
-  ulElement.appendChild(liElement);
+  mainContent.appendChild(ulElement);
 }
 
-mainContent.appendChild(ulElement);
+reRenderList();
